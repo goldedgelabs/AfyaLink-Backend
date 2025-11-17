31e5778a58855fcc0e064af0249de4440c8e1e67
goldedgelabs <goldedgelabs@gmail.com>
2025-11-17 16:35:20 +0300

    Create edge_network.go

---
+++ backend/workers-go/training/edge_network.go
@@ -0,0 +1,8 @@
+package training
+
+import "log"
+
+func EdgeNetworkSync() {
+	log.Println("Syncing anonymized data with Edge Learning Network...")
+	// Placeholder: upload anonymized data to master server
+}
